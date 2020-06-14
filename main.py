827680534b26f3d407795c987368bce2456da743
antoine.couacault <antoine.couacault@pc3.home>
2020-06-14 17:15:21 +0200

    feat(password_list): add menu_bar permit to open password storage

--- main.py
+++ main.py
@@ -5,6 +5,23 @@ from random import choice
 
 #constante var
 BG_COLOR = "#5c3314"
+SAVE_PATH = "password.txt"
+SECRET_KEY = 12
+
+#encode / decode msg (for password)
+def my_encode(msg):
+    encoded = ""
+
+    for let in msg:
+        encoded += chr(ord(let) + SECRET_KEY)
+    return encoded
+
+def my_decode(msg):
+    decoded = ""
+
+    for let in msg:
+        decoded += chr(ord(let) - SECRET_KEY)
+    return decoded
 
 #class with button commands
 class Button_cmd():
@@ -18,6 +35,11 @@ class Button_cmd():
         password = "".join(choice(possible_chars) for let in range(length))
         app.paswd_entry.delete(0, END)
         app.paswd_entry.insert(0, password)
+        app.cur_pass = password
+
+    def save_password(self):
+        with open(SAVE_PATH, "a+") as file:
+            file.write(my_encode(app.cur_pass) + "\n")
 
 #create main window
 class Window():
@@ -26,6 +48,7 @@ class Window():
         self.wind = Tk()
         self.config_window()
         self.command = Button_cmd()
+        self.cur_pass = ""
 
         #center with text entry and button
         self.central_fram = Frame(bg=BG_COLOR)
@@ -49,6 +72,8 @@ class Window():
         self.set_paswd_lb()
         self.set_paswd_entry()
         self.set_generate_button()
+        self.set_save_button()
+        self.set_menu_bar()
     
     def set_title(self):
         lb_title = Label(self.wind, text='Password Generator', bg=BG_COLOR, fg="white", font=("arial", 40))
@@ -66,7 +91,50 @@ class Window():
             command=self.command.generate_password)
         generate_button.grid(row = 2, column = 0)
 
+    def set_save_button(self):
+        save_button = Button(self.central_fram, text = 'Save password', font=("arial", 24), bg=BG_COLOR,
+            command=self.command.save_password)
+        save_button.grid(row = 4, column = 0)
+
+    def set_menu_bar(self):
+        menu_bar = Menu(self.wind)
+
+        file_menu = Menu(menu_bar, tearoff=0)
+        file_menu.add_command(label="Generate", command=self.command.generate_password)
+        file_menu.add_command(label="My password", command=open_password_list)
+        file_menu.add_separator()
+        file_menu.add_command(label="Quit", command=self.wind.quit)
+        menu_bar.add_cascade(label="File", menu=file_menu)
+
+        self.wind.config(menu=menu_bar)
+
+
+"""
+Window tho see our password
+"""
+
+def display_password(top):
+    lines = []
+
+    with open(SAVE_PATH, "r") as file:
+        lines = file.readlines()
+    for idx, line in enumerate(lines):
+        paswd = my_decode(line[:-1])
+        list_passwd_lb = Label(top, text = f'Password : {paswd}', bg="blue", font=("arial", 12))
+        list_passwd_lb.grid(row = idx, column = 0)
+    
+
+def open_password_list():
+    top = Toplevel(app.wind)
+    top.title("My password")
+    top.geometry("480x300")
+    top.config(background="blue")
+
+    display_password(top)
+    top.mainloop()
+
+app = Window()
+pass_list = []
 
 #mainloop
-app = Window()
 app.wind.mainloop()
